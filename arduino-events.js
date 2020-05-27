f0585428e9fffcacb5c7fe7ca1e392f3c4f20362
Dominic Pajak <8bitkick@users.noreply.github.com>
2020-05-27 11:03:04 -0700

    Add files via upload

---
+++ arduino-events.js
@@ -0,0 +1,141 @@
+
+const serialMonitor = document.getElementById('serialMonitor');
+
+class ArduinoEvents {
+  constructor(lookup) {
+    this.handler = null; // the next function we will call
+    this.parameters = []; // stack for saved parameters
+    this.lookup  = lookup;
+    // Start Web Serial (Chrome only at this time)
+    this.serial  = new WebSerial();
+    this.parser = this.parser.bind(this);
+    this.serial.setHandler(this.parser);
+  }
+
+  parser(text){
+    // remove trailing and leading whitespace
+    text = text.trim();
+
+    serialMonitor.textContent += text + Array(40-text.length).join(" ")
+
+    if (text=="") {return;}
+
+    let func = null; // function to call based on the Arduino serial output we see
+    let theseParameters = []; // parameters we only use this time around
+    let lookup = this.lookup;
+
+    // Check if the text matches any functions we expect.
+    let n = 0;
+    while (n < lookup.length && func == null) {
+      let l = lookup[n];
+      if (text.slice(0,l.message.length) == l.message) {
+        func = l.handler;
+        text = text.slice(l.message.length);
+      }
+      n++;
+    }
+
+    // anything we're left with is parameters
+    if (text.length > 0){
+      theseParameters = text.trim().split(',');
+    }
+
+    // if we've encountered a new function store it
+    if (func != null) {
+      this.handler = func;
+      this.parameters = theseParameters; // parameters in-line with a function persist
+    } else {
+      // if no function, assume this line is parameters for the last function we saw
+      theseParameters = this.parameters.concat(theseParameters);
+    }
+
+    // Parser is aware of how many parameters the function needs
+    if (this.handler != null && theseParameters.length == this.handler.length){
+      this.handler.apply(this,theseParameters); // call handler(theseParameters)
+      serialMonitor.textContent += this.handler.name+"("+theseParameters.join(",")+")\n";
+    } else {serialMonitor.textContent += "\n";}
+  }
+}
+
+
+class WebSerial {
+  constructor() {
+    this.port  = null;
+    this.handler = function (a) {console.log("Unhandled serial input: "+a)};
+    // Set up the serial connect button
+    this.button = document.getElementById('connectButton');
+    this.connect = this.connect.bind(this);
+    document.addEventListener('DOMContentLoaded', () => {
+      this.button.addEventListener('click', this.connect);
+      if (!'serial' in navigator) {
+        alert('Web Serial not supported by this browser. Try using Chrome')
+      }
+    });
+  }
+
+  async connect() {
+
+    const requestOptions = {
+      filters: [{ usbVendorId: 0x2341 }] // Filter on devices with the Arduino USB vendor ID.
+    };
+
+    this.port = await navigator.serial.requestPort(requestOptions);
+    await this.port.open({ baudrate: 9600 });
+
+    let decoder     = new TextDecoderStream();
+    this.inputDone   = this.port.readable.pipeTo(decoder.writable);
+    let inputStream  = decoder.readable;
+
+    this.reader = inputStream.getReader();
+    this.readLoop();
+  }
+
+  async disconnect() {
+    if (reader) {
+      await this.reader.cancel();
+      await this.inputDone;
+      this.reader = null;
+      this.inputDone = null;
+    }
+    await this.port.close();
+    this.port = null;
+    button.style.backgroundColor="grey";
+  }
+
+  async readLoop() {
+    let textBuffer = ""
+    while (true) {
+      const { value, done } = await this.reader.read();
+      if (value) {
+        textBuffer += value;
+        let lines = textBuffer.split('\n');
+        for (let l=0; l<lines.length-1;l++){
+          this.handler(lines[l]);
+        }
+        textBuffer=lines.pop();
+      }
+      if (done) {
+        this.reader.releaseLock();
+        break;
+      }
+    }
+  }
+
+  setHandler(fn) {
+    this.handler = fn;
+  }
+}
+
+
+function test(func){
+  let testInput = ["Show me an example apple","0.27,0.36,0.36","0.22,0.44,0.33","0.22,0.44,0.33","0.25,0.37,0.37","0.22,0.44,0.33","0.29,0.43,0.29","0.25,0.37,0.37","0.25,0.37,0.37","0.25,0.37,0.37","0.25,0.37,0.37","0.29,0.43,0.29","0.25,0.37,0.37","0.25,0.37,0.37","0.22,0.44,0.33","0.29,0.43,0.29","0.25,0.37,0.37","0.17,0.50,0.33","0.25,0.37,0.37","0.25,0.37,0.37","0.25,0.37,0.37","0.25,0.37,0.37","0.17,0.50,0.33","0.14,0.43,0.43","0.17,0.50,0.33","0.25,0.37,0.37","0.17,0.50,0.33","0.25,0.37,0.37","0.20,0.40,0.40","0.17,0.50,0.33","0.17,0.50,0.33","Show me an example orange","0.46,0.34,0.20","0.47,0.32,0.20","0.48,0.33,0.18","0.49,0.32,0.19","0.50,0.31,0.19","0.50,0.32,0.18","0.50,0.33,0.17","0.50,0.33,0.17","0.52,0.29,0.19","0.50,0.33,0.17","0.52,0.30,0.17","0.50,0.32,0.18","0.50,0.31,0.19","0.48,0.33,0.19","0.48,0.32,0.20","0.48,0.31,0.21","0.50,0.33,0.17","0.50,0.32,0.18","0.50,0.32,0.18","0.52,0.30,0.17","0.48,0.32,0.20","0.50,0.32,0.18","0.50,0.31,0.19","0.50,0.32,0.18","0.48,0.32,0.20","0.50,0.30,0.20","0.50,0.32,0.18","0.50,0.30,0.20","0.50,0.32,0.18","0.47,0.32,0.21","Show me an example peach","0.48,0.29,0.24","0.50,0.25,0.25","0.56,0.22,0.22","0.45,0.27,0.27","0.50,0.30,0.20","0.54,0.23,0.23","0.50,0.25,0.25","0.50,0.25,0.25","0.50,0.25,0.25","0.50,0.25,0.25","0.50,0.25,0.25","0.50,0.25,0.25","0.54,0.23,0.23","0.45,0.27,0.27","0.54,0.23,0.23","0.50,0.25,0.25","0.50,0.25,0.25","0.45,0.27,0.27","0.56,0.22,0.22","0.50,0.25,0.25","0.56,0.22,0.22","0.50,0.25,0.25","0.56,0.22,0.22","0.50,0.25,0.25","0.45,0.27,0.27","0.50,0.25,0.25","0.50,0.25,0.25","0.56,0.22,0.22","0.45,0.27,0.27","0.50,0.25,0.25","Let me guess your object","0.45,0.27,0.27","3","","","","","","","","","","","","","","","","","","","","","","","","","0.2,0.3,0.2","","","","","","","","","","","","","","","","","","","","","","","","","0.5,0.3,0.20"];
+  //,0.17,0.50,0.33
+  const delayLoop = (fn, delay) => {
+    return (line, i) => {
+      setTimeout(() => {
+        func(line);
+      }, i * 10);
+    }
+  };
+  testInput.forEach(delayLoop(func, 10));
+}
---
+++ sketch.js
@@ -0,0 +1,142 @@
+/*
+
+k-NN visualizer
+Companion to Color_Classifier.ino
+
+*/
+
+// Runs JS function on receiving the following Arduno input
+let arduino   = new ArduinoEvents([
+  {message:"Arduino k-NN color classifier", handler: reset},
+  {message:"Enter an object name:",         handler: addCategory},
+  {message:"Show me an example ",           handler: addExample},
+  {message:"Let me guess your object",      handler: guessObject}
+]);
+
+test(arduino.parser);
+let k        = 10;
+let examples = [];
+let categories = [];
+let countPerCategory = [];
+let guess = null;
+
+let size     = 20;
+let minx     = 180;
+let maxx     = minx+(9*size);
+let miny     = 50;
+let dot_x    = minx-size;
+let dot_y    = miny;
+let category = 0;
+
+function reset() {
+}
+
+function addCategory(a) {
+}
+
+function guessObject(red,green,blue) {
+  guess = KNearestNeighbor([red,green,blue])
+}
+
+function addExample(exampleCategory,red,green,blue){
+
+  // Calculate position
+  dot_x += size; // new dot
+  if (dot_x > maxx) {dot_x = minx; dot_y += size;} // new row
+  if (category != exampleCategory) {dot_x = minx; dot_y += size*3;
+  category=exampleCategory; categories.push({name:exampleCategory,ypos:dot_y});// new category
+  }
+
+  examples.push({
+    inputs: [red,green,blue],
+    category: exampleCategory,
+
+    x: dot_x,
+    y: dot_y,
+    nearest: false,
+  })
+}
+
+function setup() {
+  var canvas = createCanvas(500, 500);
+  canvas.parent('p5-sketch');
+  strokeWeight(2);
+  textSize(24);
+  textAlign(LEFT);
+}
+
+function draw() {
+  background(0x88);
+
+  categories.forEach(categoryLabel)
+
+  examples.forEach(drawDot);
+
+  function drawDot(example){
+    // Normalize color for illustration
+    var r = example.inputs[0];
+    var g = example.inputs[1];
+    var b = example.inputs[2];
+    var f = 255/Math.max(r,g,b)
+
+    if (example.nearest) {stroke("white");} else {noStroke();}
+    fill(color(r*f,g*f,b*f));
+    ellipse(example.x,example.y,size-1,size-1);
+  }
+
+  function categoryLabel(cat){
+    for (let c = 0; c<categories.length; c++){
+      textSize(80);
+      if (guess == categories[c].name) {
+        fill('#bbb')
+        rect(minx-size,categories[c].ypos-size*2,maxx,size*5)
+        fill('#fff');
+      } else {
+        fill('#bbb');
+      }
+
+      text(countPerCategory[c], maxx+size*2, categories[c].ypos+48);
+      textSize(16);
+      text(categories[c].name, minx-8, categories[c].ypos-22);
+    }
+  }
+}
+
+// Calculate KNN
+// Just for vizualization purposes - Arduino is calculating the same on-device
+function KNearestNeighbor(subject){
+
+  function distance(a, b) {
+    return Math.hypot(b[0]-a[0], b[1]-a[1], b[2]-a[2]);
+  }
+  var KNearest = [], max = 0, maxCategory = null
+
+  // Sort examples by distance from subject
+  var sorted = examples.sort(
+    (a, b) =>   distance(a.inputs, subject)
+              - distance(b.inputs, subject)
+  );
+
+  // Put the k nearest in the KNearest list
+  for (let i = 0; i<sorted.length;i++){
+    if (i < k) {
+      sorted[i].nearest = true;
+      KNearest.push(sorted[i]);
+    } else {
+      sorted[i].nearest = false;
+    }
+  }
+
+  examples = sorted; // store back to global array
+
+  // Count the number from each catgory in KNearest
+  countPerCategory = [];
+  categories.forEach(function(thisCategory) {
+    var count = KNearest.filter(d => d.category === thisCategory.name).length;
+    if (count > max) {
+      maxCategory = thisCategory.name;
+      max=count;}
+      countPerCategory.push(count);
+    })
+    return maxCategory; // guess is category with the greatest number of closest examples
+  }
